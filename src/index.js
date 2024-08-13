173782ae6e20b1fd6bdce8d3aa821c0b44065a2a
paugal <pau.galan22@gmail.com>
2024-08-13 15:40:12 +0200

    setup pages 2

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import store from "./store/store"
 import { Provider } from "react-redux";
+import { HashRouter as Router } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </StrictMode>
 );
