f8b74da3c06808ed8d4a28e53d0ea5212c715d90
paugal <pau.galan22@gmail.com>
2024-08-13 16:34:52 +0200

    api key

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -114,7 +114,7 @@ export default function Map() {
             >
                 <TileLayer
                     attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
+                    url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${process.env.STADIA_MAP_KEY}`}
                 />
                 <MarkerClusterGroup
                     chunkedLoading
