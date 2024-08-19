b5e1e0ebdddcf58e63bbd75f38fe67404439aef1
paugal <pau.galan22@gmail.com>
2024-08-19 16:18:38 +0200

    fix filters

--- src/components/MarkerListCard/MarkerListCard.js
+++ src/components/MarkerListCard/MarkerListCard.js
@@ -5,7 +5,7 @@ import "./MarkerListCard.css";
 export default function MarkerListCard() {
   const markers = useSelector((state) => state.markers);
   return (
-    <div className="card">
+    <div className="card-filters">
       <h2>Filters</h2>
       {/* <ul>
         {markers.map((marker, index) => (
@@ -15,17 +15,32 @@ export default function MarkerListCard() {
         ))}
       </ul> */}
       <h4>Mode</h4>
-      <input type="checkbox" name="bus" id="bus" />{" "}
-      <label htmlFor="bus">Bus</label>
-      <input type="checkbox" name="metro" id="metro" />{" "}
-      <label htmlFor="metro">Metro</label>
-      <input type="checkbox" name="tram" id="tram" />{" "}
-      <label htmlFor="tram">Tram</label>
+      <div>
+        <input type="checkbox" name="bus" id="bus" />{" "}
+        <label htmlFor="bus">Bus</label>
+      </div>
+      <div>
+        <input type="checkbox" name="metro" id="metro" />{" "}
+        <label htmlFor="metro">Metro</label>
+      </div>
+      <div>
+        <input type="checkbox" name="tram" id="tram" />{" "}
+        <label htmlFor="tram">Tram</label>
+      </div>
+
       <h4>Date</h4>
-      <label htmlFor="from">From</label>
-      <input type="datetime-local" name="from" id="from" />
-      <label htmlFor="to">To</label>
-      <input type="datetime-local" name="to" id="to" />
+      <div>
+        <label htmlFor="from">From</label>
+        <input type="datetime-local" name="from" id="from" />
+      </div>
+      <div>
+        <label htmlFor="to">To</label>
+        <input type="datetime-local" name="to" id="to" />
+      </div>
+      <div>
+        <button> Reset filters</button>
+        <button> Hide filters</button>
+      </div>
     </div>
   );
 }
