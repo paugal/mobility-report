04f1f83474a2dc80838bc009d8c2505a367c6c3a
paugal <pau.galan22@gmail.com>
2024-08-19 16:15:41 +0200

    working filters

--- src/components/MarkerListCard/MarkerListCard.js
+++ src/components/MarkerListCard/MarkerListCard.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from "react";
 import { useSelector } from "react-redux";
-import "./MarkerListCard.css"
-
+import "./MarkerListCard.css";
 
 export default function MarkerListCard() {
-    const markers = useSelector((state) => state.markers);
-    return (
-        <div className='card'>
-            <h3>Markers</h3>
-            <ul>
-                {markers.map((marker, index) => (
-                    <li key={index}>
-                        <strong>Marker {index + 1}:</strong> {marker.type}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const markers = useSelector((state) => state.markers);
+  return (
+    <div className="card">
+      <h2>Filters</h2>
+      {/* <ul>
+        {markers.map((marker, index) => (
+          <li key={index}>
+            <strong>Marker {index + 1}:</strong> {marker.type}
+          </li>
+        ))}
+      </ul> */}
+      <h4>Mode</h4>
+      <input type="checkbox" name="bus" id="bus" />{" "}
+      <label htmlFor="bus">Bus</label>
+      <input type="checkbox" name="metro" id="metro" />{" "}
+      <label htmlFor="metro">Metro</label>
+      <input type="checkbox" name="tram" id="tram" />{" "}
+      <label htmlFor="tram">Tram</label>
+      <h4>Date</h4>
+      <label htmlFor="from">From</label>
+      <input type="datetime-local" name="from" id="from" />
+      <label htmlFor="to">To</label>
+      <input type="datetime-local" name="to" id="to" />
+    </div>
+  );
 }
-
-
-
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,16 @@
-import { useEffect, useState, useRef  } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import "./Map.css"
+import "./Map.css";
 
 //Leaflet
-import { MapContainer, Marker, TileLayer, Popup, useMapEvents, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Icon, divIcon, point } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
@@ -11,29 +18,28 @@ import MarkerClusterGroup from "react-leaflet-cluster";
 import pointerImage from "../../assets/pointer.png";
 import pointerSvg from "../../assets/svgpointer.svg";
 
-import pointerBus from "../../assets/pointers/train.svg"
-import pointerMetro from "../../assets/pointers/metro.svg"
-import pointerPedestrian from "../../assets/pointers/pedestrian.svg"
-import pointerTrain from "../../assets/pointers/train.svg"
-import pointerTram from "../../assets/pointers/tram.svg"
-
+import pointerBus from "../../assets/pointers/train.svg";
+import pointerMetro from "../../assets/pointers/metro.svg";
+import pointerPedestrian from "../../assets/pointers/pedestrian.svg";
+import pointerTrain from "../../assets/pointers/train.svg";
+import pointerTram from "../../assets/pointers/tram.svg";
 
 //Store
 import { setMarkers, fetchMarkers, addMarker } from "../../store/markersSlice";
 /* import { addMarkerRealtime, updateMarkerRealtime, deleteMarkerRealtime } from "../../store/markerSlice";*/
 
 //Components
-import PopUpMarker from "../PopUpMarker/PopUpMarker"
+import PopUpMarker from "../PopUpMarker/PopUpMarker";
 import MyLocation from "../MiLocation/MyLocation";
-import {getUserLocation} from "../../lib/util/util"
+import { getUserLocation } from "../../lib/util/util";
+import MarkerListCard from "../MarkerListCard/MarkerListCard";
 
+function InsideMap() {
+  const markers = useSelector((state) => state.markers);
+  const dispatch = useDispatch();
 
-function InsideMap(){
-    const markers = useSelector((state) => state.markers);
-    const dispatch = useDispatch();
-
-    /*REALTIME*/
-    /* useEffect(() => {
+  /*REALTIME*/
+  /* useEffect(() => {
         // Fetch initial markers
         dispatch(fetchMarkers());
 
@@ -65,130 +71,133 @@ function InsideMap(){
         };
     }, [dispatch]); */
 
-    useEffect(() => {
-        dispatch(fetchMarkers());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchMarkers());
+  }, [dispatch]);
 
-    const customIcon = new Icon({
-        iconUrl: pointerSvg,
-        iconSize: [38, 38],
+  const customIcon = new Icon({
+    iconUrl: pointerSvg,
+    iconSize: [38, 38],
+  });
+
+  const getCustomIcon = (type) => {
+    let iconUrl;
+    switch (type) {
+      case "Bus":
+        iconUrl = pointerBus;
+        break;
+      case "Metro":
+        iconUrl = pointerMetro;
+        break;
+      case "Pedestrian":
+        iconUrl = pointerPedestrian;
+        break;
+      case "Train":
+        iconUrl = pointerTrain;
+        break;
+      case "Tram":
+        iconUrl = pointerTram;
+        break;
+      default:
+        iconUrl = pointerMetro; // Fallback icon
+    }
+    return new Icon({
+      iconUrl,
+      iconSize: [38, 38],
     });
+  };
 
-    const getCustomIcon = (type) => {
-        let iconUrl;
-        switch (type) {
-            case "Bus":
-                iconUrl = pointerBus;
-                break;
-            case "Metro":
-                iconUrl = pointerMetro;
-                break;
-            case "Pedestrian":
-                iconUrl = pointerPedestrian;
-                break;
-            case "Train":
-                iconUrl = pointerTrain;
-                break;
-            case "Tram":
-                iconUrl = pointerTram;
-                break;
-            default:
-                iconUrl = pointerMetro; // Fallback icon
-        }
-        return new Icon({
-            iconUrl,
-            iconSize: [38, 38],
-        });
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+      className: "custom-marker-cluster",
+      iconSize: point(33, 33, true),
+    });
+  };
+
+  const handleMapClick = (e) => {
+    const { lat, lng } = e.latlng;
+    const newMarker = {
+      latitude: lat,
+      longitude: lng,
+      type: `Metro`,
     };
-
-    const createCustomClusterIcon = (cluster) => {
-        return new divIcon({
-            html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
-            className: "custom-marker-cluster",
-            iconSize: point(33, 33, true)
-        });
-    };
-
-    const handleMapClick = (e) => {
-        const { lat, lng } = e.latlng;
-        const newMarker = {
-            latitude: lat,
-            longitude: lng,
-            type: `Metro`
-        };
-        /* dispatch(setMarkers([...markers, newMarker])); */
-        dispatch(addMarker({
-            latitude: lat,
-            longitude: lng,
-            type: newMarker.type
-        }));
-    };
-
-    const MapEventsHandler = ({ handleMapClick }) => {
-        useMapEvents({
-            click: (e) => handleMapClick(e),
-        });
-        return null;
-    };
-
-    const CustomZoomControl = () => {
-        const map = useMap();
-
-        useEffect(() => {
-            map.zoomControl.setPosition('bottomright');
-        }, [map]);
-
-        return null;
-    };
-    return (
-        <>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${process.env.REACT_APP_STADIA_MAP_KEY}`}
-                />
-                <MarkerClusterGroup
-                    chunkedLoading
-                    iconCreateFunction={createCustomClusterIcon}
-                >
-                    {markers.map(marker => (
-                        <Marker key={marker.id} position={[marker.latitude, marker.longitude]}
-                            icon={getCustomIcon(marker.type)}>
-                            <PopUpMarker data={marker} />
-                        </Marker>
-                    ))}
-                    
-                </MarkerClusterGroup>
-                <MyLocation/>
-                <MapEventsHandler handleMapClick={handleMapClick} />
-                <CustomZoomControl />
-                </>
+    /* dispatch(setMarkers([...markers, newMarker])); */
+    dispatch(
+      addMarker({
+        latitude: lat,
+        longitude: lng,
+        type: newMarker.type,
+      })
     );
+  };
+
+  const MapEventsHandler = ({ handleMapClick }) => {
+    useMapEvents({
+      click: (e) => handleMapClick(e),
+    });
+    return null;
+  };
+
+  const CustomZoomControl = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      map.zoomControl.setPosition("bottomright");
+    }, [map]);
+
+    return null;
+  };
+  return (
+    <>
+      <TileLayer
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${process.env.REACT_APP_STADIA_MAP_KEY}`}
+      />
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createCustomClusterIcon}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            position={[marker.latitude, marker.longitude]}
+            icon={getCustomIcon(marker.type)}
+          >
+            <PopUpMarker data={marker} />
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
+      <MyLocation />
+      <MapEventsHandler handleMapClick={handleMapClick} />
+      <CustomZoomControl />
+    </>
+  );
 }
 
-export default function Map({userLocation}) {
-    
-    const mapRef = useRef();
+export default function Map({ userLocation }) {
+  const mapRef = useRef();
 
-    useEffect(() => {
-        if (userLocation && mapRef.current) {
-            const map = mapRef.current;
-            map.setView([userLocation.latitude, userLocation.longitude], 13); // Center the map and set zoom level
-        }
-    }, [userLocation]);
-    
-    return (
-        <div style={{ position: 'relative', flexGrow: "1", display: "flex" }}>
-            <MapContainer
-                center={[41.3870, 2.1700]}
-                zoom={13}
-                minZoom={0}
-                maxZoom={20}
-                style={{ flexGrow: 1, height: "100%", width: "100%" }}
-                ref={mapRef}
-            >
-                <InsideMap></InsideMap>
-            </MapContainer>
+  useEffect(() => {
+    if (userLocation && mapRef.current) {
+      const map = mapRef.current;
+      map.setView([userLocation.latitude, userLocation.longitude], 13); // Center the map and set zoom level
+    }
+  }, [userLocation]);
 
-        </div>
-    );
+  return (
+    <div style={{ position: "relative", flexGrow: "1", display: "flex" }}>
+      <MapContainer
+        center={[41.387, 2.17]}
+        zoom={13}
+        minZoom={0}
+        maxZoom={20}
+        style={{ flexGrow: 1, height: "100%", width: "100%" }}
+        ref={mapRef}
+      >
+        <InsideMap></InsideMap>
+      </MapContainer>
+      <MarkerListCard></MarkerListCard>
+    </div>
+  );
 }
