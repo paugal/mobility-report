8f06686491cd5e93a0316d463328c74a0f85c8e6
paugal <pau.galan22@gmail.com>
2024-10-24 18:23:50 +0200

    feedback when submit report

--- src/pages/Report/Report.jsx
+++ src/pages/Report/Report.jsx
@@ -33,6 +33,7 @@ export default function Report() {
     longitude: 0,
     type: "Metro",
   });
+  const [send, setSend] = useState(false);
 
   useEffect(() => {
     dispatch(fetchReports());
@@ -145,111 +146,123 @@ export default function Report() {
         lng: null,
       });
       setLocation({ lat: null, lng: null });
+      setSend(true);
     } catch (error) {
       console.error("Submission error:", error);
       alert(t("submissionError"));
+      setSend(false);
     }
   };
 
   return (
     <div className="formReport">
-      <form onSubmit={submitForm} className="center-formReport">
-        <h1>{t("whatDoYouWantToReport")}</h1>
-        <div className="formGrid">
-          <div className="textColum">
-            <label htmlFor="mobility_mode" className="required-field">
-              {t("chooseMobilityMode")}
-            </label>
-            <select
-              name="mobility_mode"
-              id="mobility_mode"
-              className="required"
-              value={formData.mobility_mode}
-              onChange={handleChange}
-            >
-              <option value="" disabled>
-                {t("chooseOne")}
-              </option>
-              {Object.keys(jsonData.mobility_modes).map((mode, index) => (
-                <option key={index} value={mode}>
-                  {mode}
-                </option>
-              ))}
-            </select>
-
-            <label htmlFor="problemType" className="required-field">
-              {t("chooseProblemType")}
-            </label>
-            <select
-              name="problemType"
-              id="problemType"
-              className="required"
-              value={formData.problemType}
-              onChange={handleChange}
-              disabled={!formData.mobility_mode}
-            >
-              <option value="" disabled>
-                {t("chooseOne")}
-              </option>
-              {availableProblemTypes.map((type, index) => (
-                <option key={index} value={type}>
-                  {type}
-                </option>
-              ))}
-            </select>
-
-            <label htmlFor="details" className="required-field">
-              {t("specifyProblem")}
-            </label>
-            <select
-              name="details"
-              id="details"
-              className="required"
-              value={formData.details}
-              onChange={handleChange}
-              disabled={!formData.problemType}
-            >
-              <option value="" disabled>
-                {t("chooseOne")}
-              </option>
-              {availableDetails.map((detail, index) => (
-                <option key={index} value={detail}>
-                  {detail}
-                </option>
-              ))}
-            </select>
-
-            <label htmlFor="description" className="required-field">
-              {t("description")}
-            </label>
-            <textarea
-              name="description"
-              id="description"
-              className="required"
-              value={formData.description}
-              onChange={handleChange}
-            ></textarea>
-
-            <label htmlFor="email">{t("email")}</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder={t("emailPlaceholder")}
-            />
-            <span>{t("emailNote")}</span>
-          </div>
-          <div className="mapColum">
-            <label className="required-field">{t("selectLocation")}</label>
-            <ReportMap setLocationForm={setLocation}></ReportMap>
+      {send ? (
+        <div className="report-send">
+          <h1>{t("ThanksReport")}</h1>
+          <h2>{t("ThanksReportSub")}</h2>
+          <div className="success-animation">
+            <div className="circle-check"></div>
           </div>
         </div>
+      ) : (
+        <form onSubmit={submitForm} className="center-formReport">
+          <h1>{t("whatDoYouWantToReport")}</h1>
+          <div className="formGrid">
+            <div className="textColum">
+              <label htmlFor="mobility_mode" className="required-field">
+                {t("chooseMobilityMode")}
+              </label>
+              <select
+                name="mobility_mode"
+                id="mobility_mode"
+                className="required"
+                value={formData.mobility_mode}
+                onChange={handleChange}
+              >
+                <option value="" disabled>
+                  {t("chooseOne")}
+                </option>
+                {Object.keys(jsonData.mobility_modes).map((mode, index) => (
+                  <option key={index} value={mode}>
+                    {mode}
+                  </option>
+                ))}
+              </select>
 
-        <div className="buttonBox">
-          <button type="submit">{t("submit")}</button>
-        </div>
-      </form>
+              <label htmlFor="problemType" className="required-field">
+                {t("chooseProblemType")}
+              </label>
+              <select
+                name="problemType"
+                id="problemType"
+                className="required"
+                value={formData.problemType}
+                onChange={handleChange}
+                disabled={!formData.mobility_mode}
+              >
+                <option value="" disabled>
+                  {t("chooseOne")}
+                </option>
+                {availableProblemTypes.map((type, index) => (
+                  <option key={index} value={type}>
+                    {type}
+                  </option>
+                ))}
+              </select>
+
+              <label htmlFor="details" className="required-field">
+                {t("specifyProblem")}
+              </label>
+              <select
+                name="details"
+                id="details"
+                className="required"
+                value={formData.details}
+                onChange={handleChange}
+                disabled={!formData.problemType}
+              >
+                <option value="" disabled>
+                  {t("chooseOne")}
+                </option>
+                {availableDetails.map((detail, index) => (
+                  <option key={index} value={detail}>
+                    {detail}
+                  </option>
+                ))}
+              </select>
+
+              <label htmlFor="description" className="required-field">
+                {t("description")}
+              </label>
+              <textarea
+                name="description"
+                id="description"
+                className="required"
+                value={formData.description}
+                onChange={handleChange}
+              ></textarea>
+
+              <label htmlFor="email">{t("email")}</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder={t("emailPlaceholder")}
+              />
+              <span>{t("emailNote")}</span>
+            </div>
+            <div className="mapColum">
+              <label className="required-field">{t("selectLocation")}</label>
+              <ReportMap setLocationForm={setLocation}></ReportMap>
+            </div>
+          </div>
+
+          <div className="buttonBox">
+            <button type="submit">{t("submit")}</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
