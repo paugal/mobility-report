b8889d6f128b8942089e5685657650f7789aadcf
paugal <pau.galan22@gmail.com>
2024-10-24 19:02:09 +0200

    Header Redesign

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,41 +14,43 @@ export default function Header() {
   };
 
   return (
-    <div className="header">
-      <h1 className="montserrat-title">MORE: Mobility Report</h1>
-      <div className="navbar">
-        <Link
-          id="map"
-          to="/map"
-          className={location.pathname === "/map" ? "active" : ""}
-        >
-          {i18n.t("map")}
-        </Link>
-        <Link
-          id="report"
-          to="/report"
-          className={location.pathname === "/report" ? "active" : ""}
-        >
-          {i18n.t("report")}
-        </Link>
-        <Link
-          id="dashboard"
-          to="/dashboard"
-          className={location.pathname === "/dashboard" ? "active" : ""}
-        >
-          {i18n.t("dashboard")}
-        </Link>
-        <select
-          className="lenguageSelect"
-          defaultValue={i18n.language}
-          onChange={onChangeLang}
-        >
-          {LANGUAGES.map(({ code, label }) => (
-            <option key={code} value={code}>
-              {label}
-            </option>
-          ))}
-        </select>
+    <div className="header-container">
+      <div className="header">
+        <h1 className="montserrat-title">MORE: Mobility Report</h1>
+        <div className="navbar">
+          <Link
+            id="map"
+            to="/map"
+            className={location.pathname === "/map" ? "active" : ""}
+          >
+            {i18n.t("map")}
+          </Link>
+          <Link
+            id="report"
+            to="/report"
+            className={location.pathname === "/report" ? "active" : ""}
+          >
+            {i18n.t("report")}
+          </Link>
+          <Link
+            id="dashboard"
+            to="/dashboard"
+            className={location.pathname === "/dashboard" ? "active" : ""}
+          >
+            {i18n.t("dashboard")}
+          </Link>
+          <select
+            className="lenguageSelect"
+            defaultValue={i18n.language}
+            onChange={onChangeLang}
+          >
+            {LANGUAGES.map(({ code, label }) => (
+              <option key={code} value={code}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </div>
   );
