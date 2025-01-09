eceb9fd1059f191ad0eac1723a6a63c05a06d413
Pau Galan <50529290+paugal@users.noreply.github.com>
2025-01-09 22:50:46 +0100

    Updates

---
+++ static/js/198.34818101.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact=self.webpackChunkreact||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.34818101.chunk.js.map
