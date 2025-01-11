a65448b4ab4c02a64d5ac28edc3793641675a658
Pau Galan <50529290+paugal@users.noreply.github.com>
2025-01-11 15:00:28 +0100

    fix

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
